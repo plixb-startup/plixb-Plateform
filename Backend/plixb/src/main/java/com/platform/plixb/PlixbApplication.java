767c9b6d199e4e0186f8197deb5adbffb676defb
Fonji Terence <terencehilln@gmail.com>
2022-12-14 10:50:06 +0100

    Dashboard and backend Created

---
+++ Backend/plixb/src/main/java/com/platform/plixb/PlixbApplication.java
@@ -0,0 +1,13 @@
+package com.platform.plixb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlixbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlixbApplication.class, args);
+	}
+
+}
---
+++ Backend/plixb/src/test/java/com/platform/plixb/PlixbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.plixb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlixbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
